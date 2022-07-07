931af23b70fd0e0b4337043e512607719d7ec650
Ewan2333 <108774083+Ewan2333@users.noreply.github.com>
2022-07-07 17:01:18 +0800

    Ewan

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -51,7 +51,10 @@ const links = [
                 class="absolute top-4 left-4 z-50 h-20 w-20 overflow-hidden rounded-full bg-pink-200 shadow-md shadow-blue-300 ring-1 ring-violet-700 md:h-24 md:w-24"
                 v-if="$route.path !== '/about'"
             >
-                <img src="https://placekitten.com/320/320" class="w-full rounded-full" />
+                <img
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6J_hcVgG0gtEk8C7ApmTUZjcCx9gLrVv9jg&usqp=CAU"
+                    class="w-full rounded-full"
+                />
             </div>
         </transition>
     </div>
